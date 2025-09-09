69e24018c592d215d8c234ad5db34c440835e1fe
dev-celo <marcelohenrique.backend@gmail.com>
2025-09-09 17:02:13 -0300

    Feat: Implementando página de contato

---
+++ src/pages/PageContato/PageContato.jsx
@@ -0,0 +1,13 @@
+import ContatoChuvaNoCacau from "../../components/Contato/Contato";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+export default function PageContato() {
+  return (
+    <>
+      <Header />
+      <ContatoChuvaNoCacau />
+      <Footer />
+    </>
+  )
+}
