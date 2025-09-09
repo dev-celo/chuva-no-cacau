d7bdda3603fa04c8f40b788f114fbca4aa12ef95
dev-celo <marcelohenrique.backend@gmail.com>
2025-09-09 16:59:18 -0300

    Updt: Atualizando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import PaginaNoticias from "./pages/PageNoticias/PageNoticias";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoticiaDetalhe from "./pages/PageNoticias/NoticiaDetalhe";
+import PageSobre from "./pages/PageSobre/PageSobre";
+import PageContato from "./pages/PageContato/PageContato";
+import PageValorCacau from "./pages/PageValorCacau/PageValorCacau";
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/noticias" element={<PaginaNoticias />} />
+        <Route path="/sobre" element={<PageSobre />} />
+        <Route path="/contato" element={<PageContato />} />
+        <Route path="/contato" element={<PageContato />} />
+        <Route path="/valor-do-cacau" element={<PageValorCacau />} />
         <Route path="/noticias/:id" element={<NoticiaDetalhe />} />
       </Routes>
     </BrowserRouter>
